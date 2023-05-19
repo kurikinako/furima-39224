6ed250c45ba7d7ee45539aaa0bb774e48e9a2150
kurikinako <kurikinako.h@gmail.com>
2023-05-19 15:41:31 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates  :nickname, :birth_day, :encrypted_password
-    validates  :last_name, :first_name, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/ }
+    validates  :nickname, :birth_day
+    validates  :last_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
     validates  :last_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
   end
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]{6,}\z/ }
