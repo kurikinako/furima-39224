3018029c67964380a77ae4759028540799c3412d
kurikinako <kurikinako.h@gmail.com>
2023-05-22 22:53:35 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :require_login, only: [:new]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+    @items = Item.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path      
+    else
+      @items = Item.all
+      render :new      
+    end
+  end
+
+  private
+    def item_params
+      params.require(:item).permit(:image, :title, :version, :category_id, :condition_id, :postage_id, :sender_id, :ship_date_id, :price).merge(user_id: current_user.id)
+    end
+    
+    def require_login
+      unless user_signed_in?
+        redirect_to new_user_session_path
+    end
   end
 end
