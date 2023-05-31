4fa483d1e22a8003c700438b5711b68af55d973c
kurikinako <kurikinako.h@gmail.com>
2023-05-31 21:56:02 +0900

    order_addressモデルの単体テストコードの修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,9 +1,11 @@
 FactoryBot.define do
   factory :order_address do
+    token             {'tok_abcdefghijk00000000000000000' }
     post_code         { '123-4567' }
     sender_id         { 5 }
     city              { '東京都' }
     add_number        { '1-1' }
+    building_name     { '東京マンション' }
     tel_number        { '09012345678' }
   end
 end
