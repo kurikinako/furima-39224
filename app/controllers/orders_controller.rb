349b7050b06ede973d775cb496b28c37d04f5b73
kurikinako <kurikinako.h@gmail.com>
2023-05-31 21:55:04 +0900

    コントローラーの修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,15 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!, except: :index
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
-    if @item.order.present? && @item.user != current_user
+    if !user_signed_in? || @item.user == current_user
       redirect_to root_path
-    elsif !user_signed_in? || @item.user == current_user
-      redirect_to new_user_session_path
     end
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       Payjp.api_key = ENV['PAYJP_SECRET_KEY']
@@ -30,8 +27,12 @@ class OrdersController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def order_params
-    params.require(:order_address).permit(:user_id, :item_id, :post_code, :sender_id, :city, :add_number, :building_name, :tel_number, :token).merge(
+    params.require(:order_address).permit(:post_code, :sender_id, :city, :add_number, :building_name, :tel_number).merge(
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
   end
